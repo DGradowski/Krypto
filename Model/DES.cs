f7a8d7ed5cceeef8a73517b43efe7adf260b321f
JaGawrysiak <252325@edu.p.lodz.pl>
2025-03-28 20:37:29 +0100

    DES Finished

--- Model/DES.cs
+++ Model/DES.cs
@@ -16,8 +16,8 @@ namespace Model
         public DES()
         {
             this.v = new Values();
-            //generateDefaultMessageAndKey();
-            //generateSubKeys();
+            generateDefaultMessageAndKey();
+            generateSubKeys();
             leftSide = null;
             rightSide = null;
         }
@@ -31,7 +31,7 @@ namespace Model
         }
 
         //ustawianie bitu
-        public void setBit(byte[] data, int index,bool bit)
+        public void setBit(byte[] data, int index, bool bit)
         {
             int whichByte = index / 8;
             int whichBit = index % 8;
@@ -55,7 +55,7 @@ namespace Model
             {
                 for (int j = 0; j < 6; j++)
                 {
-                    bool bit = checkBit(input,(6*i) + j);
+                    bool bit = checkBit(input, (6 * i) + j);
                     setBit(output, (8 * i) + j, bit);
                 }
             }
@@ -87,22 +87,25 @@ namespace Model
         }
 
         //Konkatenacja 2 bajtów
-        public byte[] concatenateBytes(byte[] byteA, byte[] byteB)
+        public byte[] concatenateBytes(byte[] byteA, int aLength, byte[] byteB, int bLength)
         {
-            int aBits = byteA.Length * 8;
-            int bBits = byteB.Length * 8;
-            byte[] output = prepareOutput(byteA.Length, byteB.Length);
+            byte[] output = prepareOutput(aLength + bLength);  // Przygotowujemy tablicę wynikową
 
-            for (int i = 0; i < aBits; i++)
+            int i = 0;
+            // Przekopiowanie bitów z byteA
+            for (; i < aLength; i++)
             {
-                bool bit = checkBit(byteA, i);
-                setBit(output, i, bit);
+                bool bit = checkBit(byteA, i);  // Sprawdzamy bit w byteA na indeksie i
+                setBit(output, i, bit);         // Ustawiamy bit w wyniku
             }
-            for (int i = 0; i < bBits; i++)
+
+            // Przekopiowanie bitów z byteB
+            for (int j = 0; j < bLength; j++, i++)
             {
-                bool bit = checkBit(byteB, i);
-                setBit(output, aBits + i, bit);
+                bool bit = checkBit(byteB, j);  // Sprawdzamy bit w byteB na indeksie j
+                setBit(output, i, bit);         // Ustawiamy bit w wyniku
             }
+
             return output;
         }
 
@@ -116,6 +119,172 @@ namespace Model
                 setBit(output, i, bit);
             }
             return output;
+        }
+
+        public byte[] leftShift(byte[] input, int numOfShifts)
+        {
+            byte[] output = new byte[4]; // 28 bitów to 4 bajty (z uwzględnieniem paddingu)
+            int halfKeySize = 28;
+            int bitsToShift = numOfShifts % halfKeySize;
+
+            for (int i = 0; i < halfKeySize; i++)
+            {
+                bool bit = checkBit(input, (i + bitsToShift) % halfKeySize);
+                setBit(output, i, bit);
+            }
+
+            return output;
+        }
+
+        public void generateSubKeys()
+        {
+            byte[] keyPC1 = shuffleBits(key, v.keyPermutation);
+
+            byte[] c = splitBytes(keyPC1, 0, 28);
+            byte[] d = splitBytes(keyPC1, 28, 28);
+
+            subKeys = new byte[v.shifts.Length][];
+            for (int i = 0; i < v.shifts.Length; i++)
+            {
+                c = leftShift(c, v.shifts[i]);
+                d = leftShift(d, v.shifts[i]);
+                byte[] cd = concatenateBytes(c, 28, d, 28);
+                subKeys[i] = shuffleBits(cd, v.taperPermutation);
+            }
+            int x = key.Length;
+        }
+
+        public void initialPermutation()
+        {
+            msg = shuffleBits(msg, v.startPermutation);
+        }
+
+        public void composeOutput()
+        {
+            msg = concatenateBytes(rightSide, v.startPermutation.Length / 2, leftSide, v.startPermutation.Length / 2);
+            msg = shuffleBits(msg, v.endPermutation);
+        }
+
+        public byte[] doXORBytes(byte[] a, byte[] b)
+        {
+            byte[] output = new byte[a.Length];
+            for (int i = 0; i < a.Length; i++)
+            {
+                output[i] = (byte)(a[i] ^ b[i]);
+            }
+            return output;
+        }
+
+        public byte createByteFromSBox(int firstSBoxValue, int secondSBoxValue)
+        {
+            return (byte)(16 * firstSBoxValue + secondSBoxValue);
+        }
+
+        public byte[] makeSBox(byte[] input)
+        {
+            input = splitBytes(input);
+            byte[] output = new byte[input.Length / 2];
+
+            for (int i = 0, firstSBoxValue = 0; i < input.Length; i++)
+            {
+                byte sixBitsFragment = input[i];
+                int rowNumb = 2 * (sixBitsFragment >> 7 & 0x0001) + (sixBitsFragment >> 2 & 0x0001);
+                int colNumb = (sixBitsFragment >> 3) & 0x000F;
+                int secondSBoxValue = v.sBox[64 * i + 16 * rowNumb + colNumb];
+                if (i % 2 == 0)
+                {
+                    firstSBoxValue = secondSBoxValue;
+                }
+                else
+                {
+                    output[i / 2] = createByteFromSBox(firstSBoxValue, secondSBoxValue);
+                }
+            }
+            return output;
+        }
+
+        public void performSBoxPBoxAndXOR()
+        {
+            rightSide = makeSBox(rightSide);
+            rightSide = shuffleBits(rightSide, v.pBox);
+            rightSide = doXORBytes(leftSide, rightSide);
+        }
+
+        public void proceedIterations(bool ifEncrypt)
+        {
+            int iteration = subKeys.Length;
+            for (int i = 0; i < iteration; i++)
+            {
+                byte[] oldRightSide = rightSide;
+                rightSide = shuffleBits(rightSide, v.expantionPermutation);
+                if (ifEncrypt)
+                {
+                    rightSide = doXORBytes(rightSide, subKeys[i]);
+                }
+                else
+                {
+                    rightSide = doXORBytes(rightSide, subKeys[iteration - i - 1]);
+                }
+                performSBoxPBoxAndXOR();
+                leftSide = oldRightSide;
+            }
+            composeOutput();
+        }
+
+        public void divideMsg()
+        {
+            int numOfBits = (msg.Length * 8) / 2;
+
+            leftSide = splitBytes(msg, 0, numOfBits);
+            rightSide = splitBytes(msg, numOfBits, numOfBits);
+        }
+
+        public void run(bool ifEncrypt)
+        {
+            initialPermutation();
+            divideMsg();
+            proceedIterations(ifEncrypt);
+        }
+
+        public string makeProperMsgLength(string msg)
+        {
+            int overflowBytesNumb = msg.Length % 8;
+            if(overflowBytesNumb != 0)
+            {
+                for (int i = 0; i < 8 - overflowBytesNumb; i++)
+                {
+                    msg += " ";
+                }
+            }
+            return msg;
+        }
+
+        public void setMsg(byte[] msg)
+        {
+            this.msg = msg;
+        }
+
+        public void setMsg(string msg)
+        {
+            msg = makeProperMsgLength(msg);
+            this.msg = Encoding.ASCII.GetBytes(msg);
+
+        }
+
+        public byte[] getMsg()
+        {
+            return msg;
+        }
+
+        public byte[] getKey()
+        {
+            return key;
+        }
+
+        public void generateDefaultMessageAndKey()
+        {
+            key = Encoding.ASCII.GetBytes("12345678");
+            msg = Encoding.ASCII.GetBytes("abcdefgh");
         }
     }
 }
--- ModelTest/DESUnitTest.cs
+++ ModelTest/DESUnitTest.cs
@@ -2,6 +2,7 @@ using Xunit;
 using Model;
 using Newtonsoft.Json.Bson;
 using Microsoft.VisualStudio.TestPlatform.Utilities;
+using System.Text;
 
 namespace ModelTest
 {
@@ -11,7 +12,7 @@ namespace ModelTest
         public void checkBitTest()
         {
             var des = new DES();
-            byte[] data = new byte[] {0b10100000};
+            byte[] data = new byte[] { 0b10100000 };
             bool result = des.checkBit(data, 0);
             Assert.True(result);
             bool result2 = des.checkBit(data, 1);
@@ -37,10 +38,10 @@ namespace ModelTest
             byte[] output1 = des.splitBytes(input1);
             byte[] expected1 = new byte[] { 0b01010100, 0b01101000, 0b10100000, 0b11001100, 0b11001100, 0b00000000, 0b11111100, 0b11000000 };
             Assert.Equal(expected1, output1);
-            byte[] input2 = new byte[] { 0b10101010, 0b11001100};
+            byte[] input2 = new byte[] { 0b10101010, 0b11001100 };
             int index = 4;
             int length = 6;
-            byte[] output2 = des.splitBytes(input2,index,length);
+            byte[] output2 = des.splitBytes(input2, index, length);
             byte[] expected2 = new byte[] { 0b10101100 };
             Assert.Equal(expected2, output2);
         }
@@ -61,12 +62,15 @@ namespace ModelTest
         public void concatenateBytesTest()
         {
             var des = new DES();
-            byte[] byte1 = new byte[] {0x01, 0xAA, 0xB1, 0xDC};
-            byte[] byte2 = new byte[] {0x02, 0x55};
-            byte[] output = des.concatenateBytes(byte1,byte2);
-            byte[] expected = new byte[] { 0x01, 0xAA, 0xB1, 0xDC, 0x02, 0x55};
+            byte[] byte1 = new byte[] { 0b00000001, 0b10101010, 0b01010101, 0b00001111 }; 
+            byte[] byte2 = new byte[] { 0b11110000, 0b11111111 };
+
+            byte[] output = des.concatenateBytes(byte1, 32, byte2, 16);
+            byte[] expected = new byte[] { 0b00000001, 0b10101010, 0b01010101, 0b00001111, 0b11110000, 0b11111111 };
+
             Assert.Equal(expected, output);
         }
+
 
         [Fact]
         public void shuffleBitsTest()
@@ -78,5 +82,117 @@ namespace ModelTest
             byte[] expected = new byte[] { 0b11110000 };
             Assert.Equal(expected, output);
         }
+
+        [Fact]
+        public void LeftShiftTest()
+        {
+            DES des = new DES();
+            byte[] input = new byte[] { 0b11001010, 0b11110001, 0b01010010, 0b11010000 };
+            int shift = 3;
+            
+            byte[] output = des.leftShift(input, shift);
+            byte[] expected = new byte[] { 0b01010111, 0b10001010, 0b10010110, 0b11100000 };
+
+            Assert.Equal(expected, output);
+        }
+
+        [Fact]
+        public void doXORBytesTest() {
+            DES des = new DES();
+            byte[] input1 = new byte[] { 0b11001010, 0b11110001, 0b01010010, 0b11010000 };
+            byte[] input2 = new byte[] { 0b10101010, 0b11110001, 0b01010010, 0b11010000 };
+
+            byte[] output = des.doXORBytes(input1, input2);
+            byte[] expected = new byte[] { 0b01100000, 0b00000000, 0b00000000, 0b00000000 };
+
+            Assert.Equal(expected, output);
+        }
+
+        [Fact]
+        public void CreateByteFromSBoxTest()
+        {
+            DES des = new DES();
+            byte result = des.createByteFromSBox(3, 10);
+            Assert.Equal((byte)0x3A, result);
+        }
+
+        [Fact]
+        public void MakeSBoxTest()
+        {
+            DES des = new DES();
+            byte[] input = new byte[] { 0b00111111, 0b00001010, 0b11000101, 0b01110111, 0b11100001, 0b01011100 };
+
+            byte[] output = des.makeSBox(input);
+            byte[] expected = new byte[] { 0b00010101, 0b10011011, 0b10000110, 0b10111100 };
+
+            Assert.Equal(expected, output);
+        }
+
+        [Fact]
+        public void MakeProperMsgLength()
+        {
+            var des = new DES();
+
+            string msg1 = "abcdefgh"; 
+            string expected1 = "abcdefgh";
+    
+            string msg2 = "abc"; 
+            string expected2 = "abc     "; 
+
+            Assert.Equal(expected1, des.makeProperMsgLength(msg1)); // Already 8
+            Assert.Equal(expected2, des.makeProperMsgLength(msg2)); // Padded to 8
+        }
+
+        [Fact]
+        public void GetMsg()
+        {
+            var des = new DES();
+            string testMessage = "abcdefgh";
+            des.setMsg(testMessage); 
+
+            byte[] expectedMsg = new byte[] { 97, 98, 99, 100, 101, 102, 103, 104 };
+
+            byte[] result = des.getMsg();
+
+            Assert.Equal(expectedMsg, result);
+        }
+
+        [Fact]
+        public void GetKey()
+        {
+            var des = new DES();
+            byte[] expectedKey = new byte[] { 49, 50, 51, 52, 53, 54, 55, 56 };
+
+            byte[] result = des.getKey();
+
+            Assert.Equal(expectedKey, result); 
+        }
+
+        [Fact]
+        public void EncriptionDecriptionTest()
+        {
+            var des = new DES();
+            des.setMsg("abcdefgh"); 
+
+
+            byte[] expectedEncryptedMsg = new byte[] {
+            0x94, 0xd4, 0x43, 0x6b, 0xc3, 0xb5, 0xb6, 0x93
+            };
+
+            byte[] expectedDecryptedMsg = Encoding.ASCII.GetBytes("abcdefgh");
+
+            des.run(true); 
+            byte[] encryptedMsg = des.getMsg();  
+
+            Assert.Equal(expectedEncryptedMsg, encryptedMsg);
+
+            des.setMsg(encryptedMsg);  
+            des.run(false);  
+            byte[] decryptedMsg = des.getMsg();  
+
+            Assert.Equal(expectedDecryptedMsg, decryptedMsg);
+        }
+
+
     }
 }
