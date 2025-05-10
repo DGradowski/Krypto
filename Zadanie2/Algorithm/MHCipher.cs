339971699274f313b3220bc8a5c301297953e418
KossaQ <45899559+KossaQ@users.noreply.github.com>
2025-05-11 00:11:12 +0200

    Encryption

--- Zadanie2/Algorithm/MHCipher.cs
+++ Zadanie2/Algorithm/MHCipher.cs
@@ -1,12 +1,105 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Text;
 
 namespace Algorithm
 {
-    internal class MHCipher
+    public class MHCipher
     {
+        private SimpleKeyGenerator keyGen;
+        private long[] privateKey { get; set; }
+        private long[] publicKey { get; set; }
+
+        public MHCipher(SimpleKeyGenerator keyGen)
+        {
+            this.keyGen = keyGen;
+            generateKeys();
+        }
+
+        public void generateKeys()
+        {
+            privateKey = keyGen.generateDefaultPrivateKey();
+            publicKey = keyGen.generatePublicKey(privateKey);
+        }
+
+        public void generatePublicKey()
+        {
+            publicKey = keyGen.generatePublicKey(privateKey);
+        }
+
+        public long[] Encrypt(string message)
+        {
+            if (string.IsNullOrEmpty(message))
+                throw new Exception("Message is empty"); 
+
+            long[] output = new long[message.Length];
+            for (int i = 0; i < message.Length; i++)
+            {
+                output[i] = calculateEncryptedChar(message[i]);
+            }
+
+            return output;
+        }
+
+        public String decrypt(long[] cipher)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            long multiInverse = calculateMultiplierModuloInverse();
+
+            foreach (long element in cipher)
+            {
+                long encryptedChar = (multiInverse * element) % keyGen.modulus;
+                stringBuilder.Append(decryptChar(encryptedChar));
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        public char decryptChar(long encryptedChar)
+        {
+            int MSB = 0x80;
+            int outputChar = 0;
+            for (int i = 7; i >= 0 && encryptedChar > 0; i--)
+            {
+                if (encryptedChar >= privateKey[i])
+                {
+                    encryptedChar -= privateKey[i];
+                    outputChar += MSB;
+                }
+                MSB /= 2;
+            }
+            return (char)outputChar;
+        }
+
+        public long calculateMultiplierModuloInverse()
+        {
+            long[] response = extendedEuclid(keyGen.multiplier, keyGen.modulus);
+            return response[0];
+        }
+
+        public long[] extendedEuclid(long c, long d) {
+            if (d == 0)
+            {
+                return new long[] {1,0};
+            }
+            long[] response = extendedEuclid(d, c % d);
+            long a = response[1];
+            long b = response[0] - (c / d) * response[1];
+            return new long[] {a,b};
+        }
+
+        public long calculateEncryptedChar(char x)
+        {
+            long output = 0;
+            int val = x;
+            for (int i = 0; i < 8; i++)
+            {
+                if ((val & 1) == 1)
+                {
+                    output += publicKey[i];
+                }
+                val >>= 1;
+            }
+            return output;
+        }
+
     }
 }
---
+++ Zadanie2/AlgorithmTest/MHCipherTest.cs
@@ -0,0 +1,53 @@
+﻿using Algorithm;
+
+namespace AlgorithmTest
+{
+    public class MHCipherTest()
+    {
+        [Fact]
+        public void encryptDecryptTest()
+        {
+            var keyGen = new SimpleKeyGenerator(); // domyślny mnożnik i modulo
+            var cipher = new MHCipher(keyGen);
+            string message = "Hello World!";
+
+            long[] encrypted = cipher.Encrypt(message);
+            string decrypted = cipher.decrypt(encrypted);
+
+            Assert.Equal(message, decrypted);
+        }
+
+        [Fact]
+        public void encryptTest()
+        {
+            var keyGen = new SimpleKeyGenerator(4571, 7187);
+            var cipher = new MHCipher(keyGen);
+            string message = "A";
+
+            long[] publicKey = keyGen.generatePublicKey(keyGen.generateDefaultPrivateKey());
+
+            List<long> expected = new();
+
+            foreach (char ch in message)
+            {
+                int val = ch;
+                long sum = 0;
+                for (int i = 0; i < 8; i++)
+                {
+                    if ((val & 1) == 1)
+                        sum += publicKey[i];
+                    val >>= 1;
+                }
+                expected.Add(sum);
+            }
+
+            long[] actual = cipher.Encrypt(message);
+
+            Assert.Equal(expected.Count, actual.Length);
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Assert.Equal(expected[i], actual[i]);
+            }
+        }
+    }
+}
